1793440f1f056c21703d0bd56dfa4ffec02160eb
matthewsidneyjacobs <matthewsidneyjacobs@gmail.com>
2016-12-21 23:01:06 -0700

    added underscore and did some cleaning up. refactor

--- server.js
+++ server.js
@@ -1,24 +1,12 @@
 var express = require('express');
 var bodyParser= require('body-parser');
+var _ =require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
 var todos= [];
 var todoNextId = 1;
-// var todos = [{
-//   id: 1,
-//   description: "meet mom for lunch",
-//   completed: false
-// },{
-//   id:2,
-//   description: 'go to market',
-//   completed: false
-// },{
-//   id:3,
-//   description: 'cat food',
-//   completed:true
-// }];
 
 app.use(bodyParser.json());
 
@@ -30,16 +18,19 @@ app.get('/', function(req, res) {
 app.get('/todos', function (req, res) {
   res.json(todos);
 });
+
 //GET /todos/:id
 app.get('/todos/:id', function (req, res) {
   var todoId = parseInt(req.params.id);
-  var matchedTodo;
-  //iterate over todos array, find the match
-  todos.forEach(function (todo) {
-    if (todoId === todo.id) {
-      matchedTodo = todo;
-    }
-  });
+  var matchedTodo = _.findWhere(todos, {id: todoId});
+  //this is what underscore can replace
+  // var matchedTodo;
+  // //iterate over todos array, find the match
+  // todos.forEach(function (todo) {
+  //   if (todoId === todo.id) {
+  //     matchedTodo = todo;
+  //   }
+  // });
 
   if (matchedTodo) {
     res.json(matchedTodo)
@@ -52,12 +43,18 @@ app.get('/todos/:id', function (req, res) {
 
 //POST /todos
 app.post('/todos', function(req, res){
-  var body = req.body;
+  var body = _.pick(req.body, 'description', 'completed'); //use _.pick to only pick description and completed
 
-  //add id field
+  if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length===0) {
+    return res.status(400).send();
+  }
+
+  //set body.description to be trimmed value
+  body.description = body.description.trim();
+
   body.id = todoNextId;
   todoNextId++;
-  //push body into array
+
   todos.push(body);
 
 
