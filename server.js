87c1555e9a9584c74abfdd6719f3ac483b2c82a9
matthewsidneyjacobs <matthewsidneyjacobs@gmail.com>
2016-12-21 21:52:50 -0700

    add post for todos

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 var express = require('express');
+var bodyParser= require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-var todos = [{
-  id: 1,
-  description: "meet mom for lunch",
-  completed: false
-},{
-  id:2,
-  description: 'go to market',
-  completed: false
-},{
-  id:3,
-  description: 'cat food',
-  completed:true
-}];
+var todos= [];
+var todoNextId = 1;
+// var todos = [{
+//   id: 1,
+//   description: "meet mom for lunch",
+//   completed: false
+// },{
+//   id:2,
+//   description: 'go to market',
+//   completed: false
+// },{
+//   id:3,
+//   description: 'cat food',
+//   completed:true
+// }];
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
   res.send('ToDo API root');
@@ -42,6 +48,20 @@ app.get('/todos/:id', function (req, res) {
   }
   // res.status(404).send();
 
+})
+
+//POST /todos
+app.post('/todos', function(req, res){
+  var body = req.body;
+
+  //add id field
+  body.id = todoNextId;
+  todoNextId++;
+  //push body into array
+  todos.push(body);
+
+
+  res.json(body);
 })
 
 app.listen(PORT, function() {
