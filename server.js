814eab67c1d0bb243d6d667ab2c45d29082ee9a3
matthewsidneyjacobs <matthewsidneyjacobs@gmail.com>
2016-12-21 23:20:49 -0700

    add delete todos route

--- server.js
+++ server.js
@@ -61,6 +61,22 @@ app.post('/todos', function(req, res){
   res.json(body);
 })
 
+//DELETE /todos/:id
+app.delete('/todos/:id', function (req, res) {
+  var todoId = parseInt(req.params.id);
+  var matchedTodo = _.findWhere(todos, {id: todoId});
+
+  if (!matchedTodo) {
+    res.status(404).json({"error": "no todo found with that id"})
+  } else {
+    todos =  _.without(todos, matchedTodo);
+    res.json(matchedTodo);
+  }
+
+  
+})
+
+
 app.listen(PORT, function() {
   console.log('listening on port: '+PORT);
 })
