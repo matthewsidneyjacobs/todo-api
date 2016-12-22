9b39baa2320ff44b81db4d79635273bf03b930af
matthewsidneyjacobs <matthewsidneyjacobs@gmail.com>
2016-12-22 10:49:11 -0700

    final static crud

--- server.js
+++ server.js
@@ -14,20 +14,22 @@ app.get('/', function(req, res) {
   res.send('ToDo API root');
 });
 
-//GET /todos?completed=true
+//GET /todos?completed=false&q=work
 app.get('/todos', function (req, res) {
   var queryParams = req.query;
   var filteredTodos = todos;
 
-  //if has property && completed === 'true'
-  //filteredTodos = _.where(filteredTodos, ?)
-  //else if has prop && completed is false
   if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
     filteredTodos = _.where(filteredTodos, {completed:true})
   } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
     filteredTodos = _.where(filteredTodos, {completed:false})
   }
 
+  if(queryParams.hasOwnProperty('q')&&queryParams.q.length>0) {
+    filteredTodos = _.filter(filteredTodos, function(todo){
+      return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) >-1;
+    });
+  }
 
   res.json(filteredTodos);
 });
