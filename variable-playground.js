216affcc7e10c2430083944dc5acd933efef2ed3
matthewsidneyjacobs <matthewsidneyjacobs@gmail.com>
2016-12-22 10:28:11 -0700

    add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,27 @@
+// var person = {
+//   name: 'andrew',
+//   age: 21
+// };
+//
+// function updatePerson (obj) {
+//   // obj = {
+//   //   name: 'Andrew',
+//   //   age: 24
+//   // }
+//   obj.age=24;
+// }
+//
+// updatePerson(person);
+// console.log(person);
+//
+
+//array example
+var cats = [1,2,3];
+
+function updateArray (arr,value) {
+  arr.push(value);
+  debugger;
+}
+
+updateArray(cats,4);
+console.log(cats);
